eec906c4cb8ec8ddefdab7b2ced7ab2812119564
canthar90 <d.cieslak90@gmail.com>
2022-05-29 22:32:24 +0200

    Added views that do things just pass the stuff from database

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world you are at the polls index") 
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    """ Shows question id """
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    """Creating response page"""
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(requestm, question_id):
+    """Creating vote page"""
+    return HttpResponse("You're voting on question %s." % question_id)

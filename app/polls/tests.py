c6df8e852c29691365909460aab296a8d478bbc6
canthar90 <d.cieslak90@gmail.com>
2022-06-05 21:44:29 +0200

    added tests for view

--- app/polls/tests.py
+++ app/polls/tests.py
@@ -45,8 +45,6 @@ class QuestionIndexViewTest(TestCase):
     def setup(self):
         self.client = Client()
 
-
-
     def test_no_questions(self):
         """If no questions exist, message is displayed"""
         response = self.client.get(reverse('polls:index'))
@@ -63,7 +61,6 @@ class QuestionIndexViewTest(TestCase):
             response.context['latest_question_list'],
             [question],
          )
-
 
     def test_future_question(self):
         """test if question with future question date are not displayed"""
@@ -92,3 +89,20 @@ class QuestionIndexViewTest(TestCase):
             response.context['latest_question_list'],
             [question2, question1]
         )
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        """test if detail view of nor published future question gives 404"""
+        future_question = create_question(question_text='Future question.',
+                                            days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """The detail view of a question with a pub_date in the past displays
+        the question text"""
+        past_question = create_question(question_text='Past question',
+                                        days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)

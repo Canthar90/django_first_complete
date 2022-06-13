dd47df5967346d6d7ad45e4f722e6a31a5adfcdc
canthar90 <d.cieslak90@gmail.com>
2022-06-13 09:42:06 +0200

    Customizing my admin site

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class Site(admin.AdminSite):
+    site_header = "Polls template side"
+    site_title = "Polls administration side"
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldset = [
+        (None,                          {'fields': ['question_text']}),
+        ('Date information',            {'fields': ['pub_date'],
+        'classes': 'collapse'}),
+        ]
+    inlines = [ChoiceInline] 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin_site = Site(name='admin')
+admin.site.register(Question, QuestionAdmin)
+
--- app/polls/models.py
+++ app/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -12,6 +13,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         """Inform if the question was published recently"""
         now = timezone.now()
